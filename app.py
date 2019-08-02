1c98c581e416b423aeedd90b7e78982333bb74e7
Tej Dangol <dangoltej@gmail.com>
2019-08-02 12:26:13 -0400

    Aded PostgreSQl and envirnoment variable and psycopy2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,8 +8,8 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
-app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///mydata.db'
+ app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///mydata.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = "TejDangol"
 api=Api(app)
